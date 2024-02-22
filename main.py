54b3abd80eee6093b7f1b50a49a2d5959e3ca964
i am doraemon <khunball46@gmail.com>
2024-02-22 13:57:42 +0700

    version 2 - hidden node 2 node

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from Nn3I2H1Oex1forStu import *
-from ucimlrepo import fetch_ucirepo
-import pandas as pd
 from Preprocess import *
 
 
 if __name__ == '__main__':
     # export_data_to_csv()
-    calculate(1000)
+    calculate(10)
